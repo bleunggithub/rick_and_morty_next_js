9b98a1e831d4ca58909af3aa91d883e4565a2465
B Leung <leungcheukkei@gmail.com>
2021-10-02 01:42:42 +0800

    update import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
 import { useRouter } from 'next/dist/client/router'
 import { useState } from 'react'
-import { Routes } from '../interface/episodes'
+import { Route } from '../interface'
 import HomeMenuOption from '../components/HomeMenuOption'
 import { HomeContainer, HomeRoot } from '../styles/Home'
 
 export default function Index() {
-  const [selectedRoute, setSelectedRoute] = useState<null | Routes>(null)
+  const [selectedRoute, setSelectedRoute] = useState<null | Route>(null)
 
   const router = useRouter()
 
-  const handleClickLink = (e: React.MouseEvent<HTMLAnchorElement>, selected: Routes) => {
+  const handleClickLink = (e: React.MouseEvent<HTMLAnchorElement>, selected: Route) => {
     e.preventDefault()
     setSelectedRoute(selected)
     router.push(selected)
